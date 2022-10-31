472d44a13fd95858b1b266a17f50a76f6de52c12
Pedro Neves <pedro.t.dasneves@gmail.com>
2022-10-31 20:04:53 -0300

    Cadastrando Produtos

---
+++ funcoes19.js
@@ -0,0 +1,13 @@
+var ids = []
+var nomes = []
+var precos = []
+var avaliacoes = []
+var index = 0
+
+
+function CadastrarProduto() {
+     ids{ index } = prompt("Insira o id do produto")
+     nomes{ index } = prompt("Insira o nome do produto")
+     precos{ index } = prompt("Insira o preço do produto")
+     avaliacoes{ idnex } = prompt("Insira a avaliaçã do produto")
+}
